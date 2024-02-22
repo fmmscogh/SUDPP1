918175e89c58931e85d326399b4b2f075602af1b
fmmscogh <154488249+fmmscogh@users.noreply.github.com>
2024-02-22 15:58:04 +0900

    플레이어 이동 및 점프,추락 구현

--- Assets/SciFi Warehouse Kit/Demo/Scripts/MouseComponent.cs
+++ Assets/SciFi Warehouse Kit/Demo/Scripts/MouseComponent.cs
@@ -19,7 +19,7 @@ public class MouseComponent : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90, 90f);
+        xRotation = Mathf.Clamp(xRotation, -45, 45f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
